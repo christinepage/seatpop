5f25fe7cfb2d490bf76462536854450c267d7541
Beverly Leu <bevali@gmail.com>
2015-03-31 00:05:43 +0000

    removing restaurant_list_items from StaticPagesController

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
     @restaurant = current_user.restaurants.build if logged_in?
-    @restaurant_list_items = current_user.restaurant_list.paginate(page: params[:page])
   end
 
   def help
